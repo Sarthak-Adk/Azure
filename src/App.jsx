49669cb4c77dd2cbf848f7ef102f370fa23fbbbf
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-09-05 12:40:23 +0545

    addinig routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import About from './pages/About';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline bg-blue-500">hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/blogs' element={<Blog/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
