71f3744ff3ebb1d147eb2d44b95d4f16c02c1009
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-09-07 12:02:37 +0545

    adding landing page design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
-import { ThreeDCardDemo } from '@/components/ui/3dCardDemo'
+import React from "react";
+import azureHeroImage from "../assets/azureLanging01.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
 const Home = () => {
   return (
     <>
-      <div className='postion-relative bg-white text-black  md:px-[140px]  transition-colors duration-500 mt-20'>
-        <p>
-          Welcome to AzureDev, your go-to platform for cutting-edge technology solutions. We specialize in web development, AI & ML, blockchain, IoT, and graphic design. Explore our portfolio to see how we can bring your ideas to life with innovation and expertise.
-        </p>
-      </div>
-      {/* features Courses section */}
-      <section className="bg-gray-100 pt-12">
-        <div className='text-center mb-10 text-4xl font-bold'>
-          <p>Features <span className='text-[#fd0d34]'>courses</span></p>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6  px-2 md:px-[140px]">
-          <ThreeDCardDemo />
-        </div>
-      </section>
-   
-    </>
-  )
-}
+      <div className="relative w-full h-[100vh] flex items-center mt-[65px] px-[135px]">
+        <div
+          className="absolute inset-0 bg-cover bg-center "
+          style={{
+            backgroundImage: `url(${azureHeroImage})`,
+            filter: "blur(3px)",
+          }}
+        ></div>
+        <div className="absolute inset-0 bg-black opacity-80"></div>
+        <div className="relative text-white w-[600px]">
+          <p className="mb-5 font-bold">NEXT GENERATION TECHNOLOGY</p>
+          <p className="font-bold text-7xl">
+            Revolutionalize The Future of{" "}
+            <span className="text-[#fd0d34]">Technology</span>
+          </p>
+          <p className="mt-8 text-xl text-gray-300">
+            We're not just building software—we're crafting digital revolutions
+            that reshape industries and redefine what's possible.
+          </p>
 
-export default Home
+          <div className="relative w-100 hidden md:block mt-10">
+            <span className="absolute inset-y-0 left-5 flex items-center text-gray-400">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </span>
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full rounded-full border bg-white pl-13 pr-27 py-[10px] text-gray-700 outline-none"
+            />
+            <button className="absolute right-1 top-1/2 transform -translate-y-1/2 bg-[#da183f] cursor-pointer text-white px-5 pb-[10px] pt-[9px] rounded-full text-sm hover:bg-[#e3586f]">
+              Search &rarr;
+            </button>
+            
+          </div>
+          <div className="mt-12 flex gap-8">
+              <button className="bg-white text-black px-8 py-2 rounded-[4px] font-semibold cursor-pointer hover:shadow-[0px_0px_5px_white]">Key Features</button>
+               <button className="border-2 border-white px-8 py-2 rounded-[4px] font-semibold cursor-pointer hover:shadow-[0px_0px_5px_white]">How We Work ? </button>
+            </div>
+        </div>
+      </div>
+      
+    </>
+  );
+};
+
+export default Home;
